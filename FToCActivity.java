1358f87eea0a501f9e55e53eef389635e7e124d3
Aidan Shanley <ashanley@csumb.edu>
2025-10-29 14:07:21 -0700

    Create FToCActivity.java

---
+++ FToCActivity.java
@@ -0,0 +1,47 @@
+package com.example.ctofconverter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.ctofconverter.databinding.ActivityFtoCactivityBinding;
+
+import java.util.Locale;
+
+public class FToCActivity extends AppCompatActivity {
+
+    private static final String CONVERTED_VALUE_EXTRA_KEY = "FToCActivity_Received_value";
+
+    ActivityFtoCactivityBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityFtoCactivityBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        double fahrenheit = getIntent().getDoubleExtra(CONVERTED_VALUE_EXTRA_KEY, 0.0);
+        binding.FtoCEditText.setText(String.format(Locale.ENGLISH, "%.2f", fahrenheit));
+
+        binding.FToCTitleTextView.setOnLongClickListener(new View.OnLongClickListener(){
+            @Override
+            public boolean onLongClick(View v){
+                Intent intent = MainActivity.MainActivityIntentFactory(getApplicationContext(), fahrenheit);
+                startActivity(intent);
+                return false;
+            }
+        });
+    }
+    public static Intent fToCIntentFactory(Context context, double fahrenheitValue){
+        Intent intent = new Intent(context, FToCActivity.class);
+        intent.putExtra(CONVERTED_VALUE_EXTRA_KEY, fahrenheitValue);
+        return intent;
+    }
+}
